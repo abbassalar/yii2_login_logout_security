6916c46dd2a9e126ddc783e679c377979924d482
abbassalar <abbassalary1365@gmail.com>
2019-06-14 01:55:43 +0430

    login-security

---
+++ advanced/common/config/params.php
@@ -0,0 +1,8 @@
+<?php
+return [
+    'adminEmail' => 'admin@example.com',
+    'supportEmail' => 'abbassalary1365@gmail.com',
+    'senderEmail' => 'noreply@example.com',
+    'senderName' => 'Example.com mailer',
+    'user.passwordResetTokenExpire' => 3600,
+];
---
+++ advanced/frontend/config/params.php
@@ -0,0 +1,4 @@
+<?php
+return [
+    'adminEmail' => 'abbassalary1365@gmail.com',
+];
